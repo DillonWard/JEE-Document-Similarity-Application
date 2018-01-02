477db9334f8082f846d931a5131d0ebee25971e7
Dillon <Dillonward13@gmail.com>
2018-01-02 20:54:48 +0000

    splitting sentences

--- src/ie/gmit/sw/ShingleGenerator.java
+++ src/ie/gmit/sw/ShingleGenerator.java
@@ -5,18 +5,33 @@ import java.util.List;
 
 public class ShingleGenerator {
 
-	public static final int CHAR_GRAM_LENGTH = 10;
+    ArrayList<String> sentenceList = new ArrayList<String>();
 	List<Integer> shingleA = new ArrayList<Integer>();
-	
+	String[] SENTENCE;
 	
 	public ShingleGenerator() {
 		super();
 	}
 
 
-	public void generateShingle(String s){
+	public void generateShingle(String line){
 		
-		shingleA.add(s.hashCode());
-		System.out.println(shingleA);
+		sentenceList.add(line);
+	    String[] sentenceArray = sentenceList.toArray(new String[sentenceList.size()]);
+
+		
+	       for (int r=0;r<sentenceArray.length;r++)
+	       {
+	           SENTENCE = sentenceArray[r].split("(?<=[.!?])\\s*");
+	           for (int i=0;i<SENTENCE.length;i++)
+	           {
+	               System.out.println("Sentence " + (i+1) + ": " + SENTENCE[i]);
+	           }
+
+	       }
+
+		//System.out.println("asdasd" + t);
 	}
+	
+
 }
